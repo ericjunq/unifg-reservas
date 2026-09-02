8be58fd758ddaed9027ed722bca04fef0a3c244d
Eric Junqueira <junqueiraeric576@gmail.com>
2026-09-02 15:49:54 -0300

    correcao na reserva

--- backend/src/main/java/ericjunq/com/github/reservas_unifg/reservas/service/ReservaService.java
+++ backend/src/main/java/ericjunq/com/github/reservas_unifg/reservas/service/ReservaService.java
@@ -3,7 +3,8 @@ package ericjunq.com.github.reservas_unifg.reservas.service;
 import ericjunq.com.github.reservas_unifg.alunos.entity.Aluno;
 import ericjunq.com.github.reservas_unifg.alunos.repository.AlunoRepository;
 import ericjunq.com.github.reservas_unifg.enums.StatusReserva;
-import ericjunq.com.github.reservas_unifg.equipamentos.service.EquipamentoService;
+import ericjunq.com.github.reservas_unifg.equipamentos.service.EquipamentosService;
+import ericjunq.com.github.reservas_unifg.equipamentos_reservados.service.EquipamentosReservadosService;
 import ericjunq.com.github.reservas_unifg.exceptions.EntityNotFoundException;
 import ericjunq.com.github.reservas_unifg.exceptions.TimeSlotAlreadyTakenException;
 import ericjunq.com.github.reservas_unifg.reservas.dto.ReservaDTO;
@@ -24,7 +25,7 @@ public class ReservaService {
     private final ReservaMapper reservaMapper;
     private final ReservaRepository reservaRepository;
     private final AlunoRepository alunoRepository;
-    private final EquipamentoService equipamentoService;
+    private final EquipamentosReservadosService equipamentoService;
 
     public Reserva criarReserva(ReservaDTO dto){
 
