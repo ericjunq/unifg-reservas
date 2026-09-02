d1a20224ebb74316a1dcbd4ca3ca45f7a80e4cd9
Eric Junqueira <junqueiraeric576@gmail.com>
2026-09-02 09:26:48 -0300

    Vinculando repositorio

---
+++ backend/src/main/java/ericjunq/com/github/reservas_unifg/ReservasUnifgApplication.java
@@ -0,0 +1,13 @@
+package ericjunq.com.github.reservas_unifg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasUnifgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasUnifgApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ericjunq/com/github/reservas_unifg/ReservasUnifgApplicationTests.java
@@ -0,0 +1,13 @@
+package ericjunq.com.github.reservas_unifg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasUnifgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
